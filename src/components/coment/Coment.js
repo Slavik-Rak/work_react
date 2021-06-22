1299b0cfa1fcbba1c25136281e803681cf43b4b4
Slavik-Rak <76218931+Slavik-Rak@users.noreply.github.com>
2021-06-23 01:06:29 +0300

    h_w_1 end

--- src/components/coment/Coment.js
+++ src/components/coment/Coment.js
@@ -1,11 +1,12 @@
+import './Coment.css'
 export default function Coment(props) {
     let {item} = props;
     return (
-        <div>
+        <div className={'wrapComent'}>
 
-            <h2>{item.name}</h2>
-            <a href={'#'}>{item.email}</a>
-            <div>{item.body}</div>
+            <h2 className={'titleComent'}>{item.name}</h2>
+            <div className={'emaiComent'} href={'#'}>{item.email}</div>
+            <div className={'descriptionComent'}>{item.body}</div>
 
         </div>
     );
