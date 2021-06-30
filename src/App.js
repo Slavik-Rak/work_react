3e2ce74745371d5b7f2d703000160470fc0443d1
Slavik-Rak <yaroslav_kart@mail.ru>
2021-06-30 09:19:52 +0300

    les_3.1_React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {useReducer} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    function reducer(state, action) {
+
+
+        switch (action.type) {
+            case  'INCREMENT_A':
+                return {...state, a: state.a + action.payload}
+            case  'INCREMENT_B':
+                return {...state, b: state.b + action.payload}
+            default:
+                return {...state}
+        }
+
+    }
+
+
+    let [state, dispatch] = useReducer(reducer, {a: 0, b: 0})
+
+    console.log(state);
+    let {a, b} = state;
+    return (
+        <div>
+
+            <button onClick={() => {
+                dispatch({type: 'INCREMENT_A', payload: 1});
+            }}>inc a - {a}  </button>
+
+            <br/>
+
+            <button onClick={() => {
+                dispatch({type: 'INCREMENT_B', payload: 10});
+            }}> inc b - {b} </button>
+
+            <br/>
+
+        </div>
+    );
 }
 
 export default App;
